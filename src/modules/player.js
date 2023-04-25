b963be391ce0bcbcb2da51a496cf9beef1520e5a
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-04-25 12:15:55 +0200

    Can get and set currentMouseCoords

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,1 +1,14 @@
-export default class Player {}
+export default class Player {
+  constructor(id = 0) {
+    this.id = id;
+    this.currentMouseCoords = null;
+  }
+
+  getCoords() {
+    return this.currentMouseCoords;
+  }
+
+  setCoords(coords) {
+    this.currentMouseCoords = coords;
+  }
+}
--- tests/player.test.js
+++ tests/player.test.js
@@ -1,1 +1,22 @@
 import Player from "../src/modules/player";
+
+describe("Player", () => {
+  describe("Set user ship's head current x and y coordinates", () => {
+    test("Set x and y coords depending on what slot the user mouse is", () => {
+      const player = new Player();
+      const x = 0;
+      const y = 0;
+      player.setCoords([x, y]);
+      expect(player.currentMouseCoords).toEqual([x, y]);
+    });
+  });
+  describe("Get user ship's head current x and y coordinates", () => {
+    test("Get x and y coords depending on what slot the user mouse is", () => {
+      const player = new Player();
+      const x = 0;
+      const y = 0;
+      player.currentMouseCoords = [x, y];
+      expect(player.getCoords()).toEqual([x, y]);
+    });
+  });
+});
