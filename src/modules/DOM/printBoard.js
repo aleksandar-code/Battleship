05b5958197112d3809c4663f62b85c8fd8cbbe09
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-04-25 17:03:10 +0200

    Create boards in the DOM

--- src/modules/DOM/printBoard.js
+++ src/modules/DOM/printBoard.js
@@ -1,5 +1,16 @@
-const printBoard = (board) => {
-  console.log(board);
+const printBoards = () => {
+  const playerBoard = document.querySelectorAll(".board");
+  for (let i = 0; i < 100; i += 1) {
+    const slot = document.createElement("div");
+    slot.classList.add("slot");
+    playerBoard[0].appendChild(slot);
+  }
+
+  for (let i = 0; i < 100; i += 1) {
+    const slot = document.createElement("div");
+    slot.classList.add("slot");
+    playerBoard[1].appendChild(slot);
+  }
 };
 
-export default printBoard;
+export default printBoards;
