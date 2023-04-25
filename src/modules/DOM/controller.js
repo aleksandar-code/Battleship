fc576c1a3cc0f83c8ca32f1d2cdcd454ebd25bc5
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-04-25 21:35:44 +0200

    Can rotate a ship in the dom once

--- src/modules/DOM/controller.js
+++ src/modules/DOM/controller.js
@@ -10,3 +10,15 @@ const ship = newGame.boards[0].ships.five;
 newGame.boards[0].changeAxis(ship);
 printBoards();
 printShips(newGame.boards);
+
+const five = document.querySelectorAll(".player .length-5");
+console.log(five);
+const arr = Array.from(five);
+
+arr.forEach((element) => {
+  element.addEventListener("click", () => {
+    newGame.boards[0].changeAxis(ship);
+    printBoards();
+    printShips(newGame.boards);
+  });
+});
