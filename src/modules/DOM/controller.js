4f39e28bbb1c3cfc617fdee4e3df9b9c584adc05
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-04-25 21:28:07 +0200

    Can print boards and rotate ships on board

--- src/modules/DOM/controller.js
+++ src/modules/DOM/controller.js
@@ -5,3 +5,8 @@ const newGame = new Game();
 newGame.gameLoop();
 printBoards();
 printShips(newGame.boards);
+
+const ship = newGame.boards[0].ships.five;
+newGame.boards[0].changeAxis(ship);
+printBoards();
+printShips(newGame.boards);
--- src/modules/DOM/printBoard.js
+++ src/modules/DOM/printBoard.js
@@ -1,20 +1,84 @@
-const printShips = (boards) => {
-  const board = document.querySelectorAll(".board");
-  const array = Array.from(board[0].children);
-  const boardData = boards[0].board.flat(Infinity);
-  console.log(array);
-  console.log(boardData);
-  boardData.forEach((node, index) => {
-    if (node.ship !== null) {
-      array[index].textContent = `ship ${node.ship.length}`;
-      console.log(index);
-      console.log(node);
+const shapeBoats = (boat) => {
+  const array = Array.from(boat);
+  array.forEach((element, index) => {
+    if (index === 0 && !element.classList.contains("rotated")) {
+      array[index].style.cssText = "border-right: none;";
+      array[index].parentNode.style.cssText = "border-right: none;";
+    } else if (
+      index === array.length - 1 &&
+      !element.classList.contains("rotated")
+    ) {
+      array[index].style.cssText = "border-left: none";
+      array[index].parentNode.style.cssText = "border-left: none;";
+    } else if (!element.classList.contains("rotated")) {
+      array[index].style.cssText = "border-left: none; border-right: none";
+      array[index].parentNode.style.cssText =
+        "border-left: none; border-right: none;";
+    } else if (index === 0 && element.classList.contains("rotated")) {
+      array[index].style.cssText = "border-bottom: none;";
+      array[index].parentNode.style.cssText = "border-bottom: none;";
+    } else if (
+      index === array.length - 1 &&
+      element.classList.contains("rotated")
+    ) {
+      array[index].style.cssText = "border-top: none";
+      array[index].parentNode.style.cssText = "border-top: none;";
+    } else if (element.classList.contains("rotated")) {
+      array[index].style.cssText = "border-top: none; border-bottom: none";
+      array[index].parentNode.style.cssText =
+        "border-top: none; border-bottom: none;";
     }
   });
 };
 
+const printShips = (boards) => {
+  const board = document.querySelectorAll(".board");
+  const array = Array.from(board[0].children);
+  const array2 = Array.from(board[1].children);
+  const boardData = boards[0].board.flat(Infinity);
+  const boardData2 = boards[1].board.flat(Infinity);
+  boardData.forEach((node, index) => {
+    if (node.ship !== null) {
+      const ship = document.createElement("div");
+      ship.classList.add(`length-${node.ship.length}`);
+      ship.classList.add("ship");
+      if (node.ship.rotated === true) ship.classList.add("rotated");
+      array[index].appendChild(ship);
+    }
+  });
+
+  boardData2.forEach((node, index) => {
+    if (node.ship !== null) {
+      const ship = document.createElement("div");
+      ship.classList.add(`length-${node.ship.length}`);
+      ship.classList.add("ship");
+      if (node.ship.rotated === true) ship.classList.add("rotated");
+      array2[index].appendChild(ship);
+    }
+  });
+  const length5 = document.querySelectorAll(".player .length-5");
+  const length4 = document.querySelectorAll(".player .length-4");
+  const length3 = document.querySelectorAll(".player .length-3");
+  const length2 = document.querySelectorAll(".player .length-2");
+
+  const lengthComputer5 = document.querySelectorAll(".computer .length-5");
+  const lengthComputer4 = document.querySelectorAll(".computer .length-4");
+  const lengthComputer3 = document.querySelectorAll(".computer .length-3");
+  const lengthComputer2 = document.querySelectorAll(".computer .length-2");
+  shapeBoats(length5);
+  shapeBoats(length4);
+  shapeBoats(length3);
+  shapeBoats(length2);
+  shapeBoats(lengthComputer5);
+  shapeBoats(lengthComputer4);
+  shapeBoats(lengthComputer3);
+  shapeBoats(lengthComputer2);
+};
+
 const printBoards = () => {
   const board = document.querySelectorAll(".board");
+  board[0].innerHTML = "";
+  board[1].innerHTML = "";
   for (let i = 0; i < 100; i += 1) {
     const slot = document.createElement("div");
     slot.classList.add("slot");
