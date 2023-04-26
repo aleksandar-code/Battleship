79b6ca56435188e795869d16af0f428fadaff0c2
aleksandar-code <83082486+aleksandar-code@users.noreply.github.com>
2023-04-26 18:12:46 +0200

    Advance on drag drop

--- src/modules/DOM/initializeGame.js
+++ src/modules/DOM/initializeGame.js
@@ -6,6 +6,56 @@ newGame.gameLoop();
 printBoards();
 printShips(newGame.boards);
 
+function dragAndDrop() {
+  const playerShips = document.querySelectorAll(".player .ship");
+  const computerShips = document.querySelectorAll(".computer .ship");
+  const playersShips = [Array.from(playerShips), Array.from(computerShips)];
+
+  playersShips.forEach((arr, index) => {
+    arr.forEach((_, i) => {
+      arr[i].setAttribute("draggable", true);
+
+      const ship = playersShips[index][i];
+      ship.ondragstart = (e) => {
+        e.preventDefault();
+      };
+      ship.style.transform = "rotate(30deg)";
+
+      ship.onmousedown = function (event) {
+        // (1) start the process
+
+        // (2) prepare to moving: make absolute and on top by z-index
+        ship.style.position = "absolute";
+        ship.style.zIndex = 1000;
+        // move it out of any current parents directly into body
+        // to make it positioned relative to the body
+        document.body.append(ship);
+        // ...and put that absolutely positioned ship under the cursor
+
+        // centers the ship at (pageX, pageY) coordinates
+        function moveAt(pageX, pageY) {
+          ship.style.left = `${pageX - ship.offsetWidth / 2}px`;
+          ship.style.top = `${pageY - ship.offsetHeight / 2}px`;
+        }
+        moveAt(event.pageX, event.pageY);
+
+        function onMouseMove(event2) {
+          moveAt(event2.pageX, event2.pageY);
+        }
+
+        // (3) move the ship on mousemove
+        document.addEventListener("mousemove", onMouseMove);
+
+        // (4) drop the ship, remove unneeded handlers
+        ship.onmouseup = function () {
+          document.removeEventListener("mousemove", onMouseMove);
+          ship.onmouseup = null;
+        };
+      };
+    });
+  });
+}
+
 (function rotateShips() {
   const realPlayerShips = Object.values(newGame.boards[0].ships);
   const realComputerShips = Object.values(newGame.boards[1].ships);
@@ -13,6 +63,7 @@ printShips(newGame.boards);
   const computerShips = document.querySelectorAll(".computer .ship");
   const playersShips = [Array.from(playerShips), Array.from(computerShips)];
   const realPlayersShips = [realPlayerShips, realComputerShips];
+  dragAndDrop();
   playersShips.forEach((arr, i) => {
     arr.forEach((element) => {
       element.addEventListener("click", () => {
@@ -21,19 +72,8 @@ printShips(newGame.boards);
         printBoards();
         printShips(newGame.boards);
         rotateShips();
+        dragAndDrop();
       });
-    });
-  });
-})();
-
-(function dragAndDrop() {
-  const playerShips = document.querySelectorAll(".player .ship");
-  const computerShips = document.querySelectorAll(".computer .ship");
-  const playersShips = [Array.from(playerShips), Array.from(computerShips)];
-
-  playersShips.forEach((arr) => {
-    arr.forEach((_, i) => {
-      arr[i].setAttribute("draggable", true);
     });
   });
 })();
--- src/modules/DOM/printBoard.js
+++ src/modules/DOM/printBoard.js
@@ -1,94 +1,54 @@
-const shapeBoats = (boat) => {
-  const array = Array.from(boat);
-  array.forEach((element, index) => {
-    if (index === 0 && !element.classList.contains("rotated")) {
-      array[index].style.cssText = "border-right: none;";
-      array[index].parentNode.style.cssText = "border-right: none;";
-    } else if (
-      index === array.length - 1 &&
-      !element.classList.contains("rotated")
-    ) {
-      array[index].style.cssText = "border-left: none";
-      array[index].parentNode.style.cssText = "border-left: none;";
-    } else if (!element.classList.contains("rotated")) {
-      array[index].style.cssText = "border-left: none; border-right: none";
-      array[index].parentNode.style.cssText =
-        "border-left: none; border-right: none;";
-    } else if (index === 0 && element.classList.contains("rotated")) {
-      array[index].style.cssText = "border-bottom: none;";
-      array[index].parentNode.style.cssText = "border-bottom: none;";
-    } else if (
-      index === array.length - 1 &&
-      element.classList.contains("rotated")
-    ) {
-      array[index].style.cssText = "border-top: none";
-      array[index].parentNode.style.cssText = "border-top: none;";
-    } else if (element.classList.contains("rotated")) {
-      array[index].style.cssText = "border-top: none; border-bottom: none";
-      array[index].parentNode.style.cssText =
-        "border-top: none; border-bottom: none;";
-    }
-  });
-};
-
 const printShips = (boards) => {
   const board = document.querySelectorAll(".board");
   const array = Array.from(board[0].children);
-  const array2 = Array.from(board[1].children);
-  const boardData = boards[0].board.flat(Infinity);
-  const boardData2 = boards[1].board.flat(Infinity);
-  boardData.forEach((node, index) => {
-    if (node.ship !== null) {
-      const ship = document.createElement("div");
-      ship.classList.add(`l${node.ship.length}`);
-      ship.classList.add("ship");
-      if (node.ship.rotated === true) ship.classList.add("rotated");
-      array[index].appendChild(ship);
-    }
+  const boardData = Object.values(boards[0].ships);
+  // const array2 = Array.from(board[1].children);
+  // const boardData2 = Object.values(boards[1].ships);
+
+  boardData.forEach((ship) => {
+    const element = document.createElement("div");
+    element.classList.add(`l${ship.length}`);
+    element.classList.add("ship");
+    if (ship.rotated === true) element.classList.add("rotated");
+    const data = { x: ship.x, y: ship.y };
+
+    array.forEach((slot, index) => {
+      if (
+        Number(slot.dataset.X) === data.x &&
+        Number(slot.dataset.Y) === data.y
+      ) {
+        array[index].appendChild(element);
+      }
+    });
   });
 
-  boardData2.forEach((node, index) => {
-    if (node.ship !== null) {
-      const ship = document.createElement("div");
-      ship.classList.add(`l${node.ship.length}`);
-      ship.classList.add("ship");
-      if (node.ship.rotated === true) ship.classList.add("rotated");
-      array2[index].appendChild(ship);
-    }
-  });
-  const length5 = document.querySelectorAll(".player .l5");
-  const length4 = document.querySelectorAll(".player .l4");
-  const length3 = document.querySelectorAll(".player .l3");
-  const length2 = document.querySelectorAll(".player .l2");
-
-  const lengthComputer5 = document.querySelectorAll(".computer .l5");
-  const lengthComputer4 = document.querySelectorAll(".computer .l4");
-  const lengthComputer3 = document.querySelectorAll(".computer .l3");
-  const lengthComputer2 = document.querySelectorAll(".computer .l2");
-  shapeBoats(length5);
-  shapeBoats(length4);
-  shapeBoats(length3);
-  shapeBoats(length2);
-  shapeBoats(lengthComputer5);
-  shapeBoats(lengthComputer4);
-  shapeBoats(lengthComputer3);
-  shapeBoats(lengthComputer2);
+  // boardData2.forEach((element) => {
+  //   const ship = document.createElement("div");
+  //   ship.classList.add(`l${element.length}`);
+  //   ship.classList.add("ship");
+  //   if (element.rotated === true) ship.classList.add("rotated");
+  //   array2.forEach((slot) => {
+  //     if (slot.dataset.X === element.x && slot.dataset.Y === element.y) {
+  //       slot.appendChild(ship);
+  //     }
+  //   });
+  // });
 };
 
 const printBoards = () => {
   const board = document.querySelectorAll(".board");
   board[0].innerHTML = "";
   board[1].innerHTML = "";
-  for (let i = 0; i < 100; i += 1) {
-    const slot = document.createElement("div");
-    slot.classList.add("slot");
-    board[0].appendChild(slot);
-  }
-
-  for (let i = 0; i < 100; i += 1) {
-    const slot = document.createElement("div");
-    slot.classList.add("slot");
-    board[1].appendChild(slot);
+  for (let i = 0; i < 2; i += 1) {
+    for (let x = 0; x < 10; x += 1) {
+      for (let y = 0; y < 10; y += 1) {
+        const slot = document.createElement("div");
+        slot.classList.add("slot");
+        slot.dataset.X = x;
+        slot.dataset.Y = y;
+        board[i].appendChild(slot);
+      }
+    }
   }
 };
 
